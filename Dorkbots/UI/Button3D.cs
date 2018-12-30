4dc708867db19144fcb4ae25be835729a094c0d5
Dayvid Jones <dayvid@dayvid.com>
2018-12-29 20:45:13 -0800

    Util update

---
+++ Dorkbots/UI/Button3D.cs
@@ -0,0 +1,40 @@
+﻿using Signals;
+using UnityEngine;
+
+namespace Dorkbots.UI
+{
+    [RequireComponent(typeof(Collider))]
+    public class Button3D : MonoBehaviour
+    {
+        public Signal<Button3D> mouseUpSignal { get; private set; }
+        public Signal<Button3D> mouseDownSignal { get; private set; }
+
+        private bool perform = true;
+
+        private void Awake()
+        {
+            mouseUpSignal = new Signal<Button3D>();
+            mouseDownSignal = new Signal<Button3D>();
+        }
+
+        void OnMouseDown()
+        {
+            if (perform) mouseDownSignal.Dispatch(this);
+        }
+
+        private void OnMouseUp()
+        {
+            if (perform) mouseUpSignal.Dispatch(this);
+        }
+
+        void OnEnable()
+        {
+            perform = true;
+        }
+
+        void OnDisable()
+        {
+            perform = false;
+        }
+    }
+}
---
+++ Dorkbots/UI/UITools.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Dorkbots.UI
+{
+    public static class UITools
+    {
+        /// <summary>
+        /// Finds the user's input position in a RectTransform</summary>
+        /// <param name="rectTransform">The target RectTransform</param>
+        /// <returns>A Vector2 object with the x and y for the position.</returns>
+        public static Vector2 GetInputVectorRectTransform(RectTransform rectTransform)
+        {
+            // Get X and Y position of the users input on the source.
+            Rect sourceRect = rectTransform.rect;
+            Vector2 localPoint;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, Input.mousePosition, Camera.main, out localPoint);
+
+            return localPoint;
+        }
+    }
+}
